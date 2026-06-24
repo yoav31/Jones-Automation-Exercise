803b90418648897c0635794f1343406cd76ee65b
yoav31 <yoav31yv@gmail.com>
2026-06-24 22:36:13 +0300

    update

--- exercise.js
+++ exercise.js
@@ -1,4 +1,4 @@
-const { chromium } = require('playwright');
+import { chromium } from 'playwright';
 (async () => {
     const browser = await chromium.launch();
     const page = await browser.newPage();
@@ -7,11 +7,12 @@ const { chromium } = require('playwright');
         const title = await page.title();
         console.log(`Page title: ${title}`);
         await page.fill('#name', 'Yoav Vaknin');
-        await page.fill('#email', 'israeli@gmail.com');
-        await page.fill('#phone', '1234567890');
+        await page.fill('#email', 'yoav31yv@gmail.com');
+        await page.fill('#phone', '0502193004');
         await page.fill('#company', 'Jones');
         await page.fill('#website', 'https://www.linkedin.com/in/yoav-vaknin-6b872a242/');
         await page.locator('#employees').selectOption({label: '51-500'});
+        await page.screenshot({ path: 'before_submit.png' });
         await page.getByRole('button', {name: 'Request a call back'}).click();
         await page.getByText('Thank You!').waitFor();
         console.log('Reached Thank You page');
