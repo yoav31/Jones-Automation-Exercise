d6438981abd8f3e0b29d63d11216706f9f265f0c
yoav31 <yoav31yv@gmail.com>
2026-06-25 20:57:38 +0300

    update

--- exercise.js
+++ exercise.js
@@ -3,8 +3,7 @@ import { chromium } from 'playwright';
     const browser = await chromium.launch();
     const page = await browser.newPage();
     try {
-        await page.goto('https://test.netlify.app/');
-        await page.waitForSelector('#name');
+        await page.goto('https://test.netlify.app/', {waitUntil: 'networkidle'});
         await page.fill('#name', 'Yoav Vaknin');
         await page.fill('#email', 'yoav31yv@gmail.com');
         await page.fill('#phone', '0502193004');
@@ -20,7 +19,6 @@ import { chromium } from 'playwright';
         console.error('Automation failed:', error);
     }
     finally {
-        await page.waitForTimeout(2000);
         await browser.close();
     }
 })();
